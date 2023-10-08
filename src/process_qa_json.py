e37ecee7bc54cc088c0ab07e8891eb36c946293f
Alberto Fernández Hernández <45654081+AlbertoUAH@users.noreply.github.com>
2023-10-08 12:50:05 +0200

    Update process_qa_json.py

--- src/process_qa_json.py
+++ src/process_qa_json.py
@@ -5,8 +5,7 @@ import json
 import re
 import os
 
-print("CURRENT DIRECTORY")
-print(os.getcwd())
+BASE_DIR = '/home/runner/work/Castena/Castena'
 
 # -- Auxiliar function to elaborate a cleaned dictionary with all questions and answers
 def elaborate_qa_dict(text):
@@ -17,12 +16,12 @@ def elaborate_qa_dict(text):
         question_answer_dictionary[question] = answer
     return question_answer_dictionary
 
-list_original_qa_json_files = os.listdir('/data/eval/')
-list_original_cleaned_qa_json_files = os.listdir('/data/eval/eval_cleaned/')
+list_original_qa_json_files = os.listdir(BASE_DIR + '/data/eval/')
+list_original_cleaned_qa_json_files = os.listdir(BASE_DIR + '/data/eval/eval_cleaned/')
 
 for original_qa_json_file in list_original_qa_json_files:
   if original_qa_json_file not in list_original_cleaned_qa_json_files:
-    df = pd.read_json(BASE_DIR + 'data/eval/' + original_qa_json_file)
+    df = pd.read_json(BASE_DIR + '/data/eval/' + original_qa_json_file)
     # Extract Q&A's (if exist)
     df['qa'] = df['annotations'].apply(lambda x: list(itertools.chain.from_iterable([[annotation_aux['value']['text'] \
                                                                                       for annotation_aux in annotation['result']] \
@@ -31,7 +30,7 @@ for original_qa_json_file in list_original_qa_json_files:
                                       )
     df['qa_dict'] = df['qa'].apply(lambda x: elaborate_qa_dict(x))
     final_json_file = list(df['qa_dict'])
-    with open('/data/eval/eval_cleaned/' + original_qa_json_file, 'w') as f:
+    with open(BASE_DIR + '/data/eval/eval_cleaned/' + original_qa_json_file, 'w') as f:
       json.dump(data, final_json_file)
 
 print("Processing finished successfully!")
