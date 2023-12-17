70c27d4c003c1c5c1595b38e896409cd4e40a7ce
Alberto Fernández Hernández <45654081+AlbertoUAH@users.noreply.github.com>
2023-12-17 18:08:01 +0100

    Update utils.py

--- src/prod/utils.py
+++ src/prod/utils.py
@@ -179,7 +179,7 @@ def setup_app(transcription_path, emb_model, model, _logger):
 
     # -- Read translated transcription
     _logger.info('Loading transcription...')
-    loader = TextLoader('albertouah/castena/main/src/prod/' + transcription_path)
+    loader = TextLoader('src/prod/' + transcription_path)
     documents = loader.load()
     # Splitting the text into chunks
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
