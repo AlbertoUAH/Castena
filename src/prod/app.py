de5d08c465d898e3f0eb9d11163a2ee21664be73
Alberto Fernández Hernández <45654081+AlbertoUAH@users.noreply.github.com>
2024-09-13 10:39:32 +0200

    Update app.py

--- src/prod/app.py
+++ src/prod/app.py
@@ -1,10 +1,11 @@
 # -- Import libraries
-from   langchain.prompts           import PromptTemplate
-from   PIL                         import Image
-from   streamlit.logger            import get_logger
-from   streamlit_player            import st_player
-import pandas                      as pd
-import streamlit                   as st
+from   langchain.prompts             import PromptTemplate
+from   PIL                           import Image
+from   streamlit.logger              import get_logger
+from   streamlit_player              import st_player
+from   langchain.tools               import DuckDuckGoSearchRun
+import pandas                        as pd
+import streamlit                     as st
 import urllib.request
 import argparse
 import together
@@ -21,15 +22,14 @@ st.set_page_config(layout="wide")
 def get_args():
     # -- 1. Setup arguments
     parser = argparse.ArgumentParser()
-    # https://raw.githubusercontent.com/AlbertoUAH/Castena/main/prompts/default_system_prompt.txt
-    parser.add_argument('--DEFAULT_SYSTEM_PROMPT_LINK', type=str, default="./prompts/default_system_prompt.txt", help='Valor para DEFAULT_SYSTEM_PROMPT_LINK')
-    parser.add_argument('--PODCAST_URL_VIDEO_PATH', type=str, default="./data/podcast_youtube_video.csv", help='Valor para PODCAST_URL_VIDEO_PATH')
+    parser.add_argument('--DEFAULT_SYSTEM_PROMPT_LINK', type=str, default="https://raw.githubusercontent.com/AlbertoUAH/Castena/main/prompts/default_system_prompt.txt", help='Valor para DEFAULT_SYSTEM_PROMPT_LINK')
+    parser.add_argument('--PODCAST_URL_VIDEO_PATH', type=str, default="https://raw.githubusercontent.com/AlbertoUAH/Castena/main/data/podcast_youtube_video.csv", help='Valor para PODCAST_URL_VIDEO_PATH')
     parser.add_argument('--TRANSCRIPTION', type=str, default='worldcast_roberto_vaquero', help='Name of the trascription')
     parser.add_argument('--MODEL', type=str, default='togethercomputer/llama-2-13b-chat', help='Model name')
     parser.add_argument('--EMB_MODEL', type=str, default='sentence-transformers/paraphrase-multilingual-mpnet-base-v2', help='Embedding model name')
+    os.system("python -m spacy download es_core_news_lg")
 
     # -- 2. Setup env and logger
-    os.environ["TOGETHER_API_KEY"] = "6101599d6e33e3bda336b8d007ca22e35a64c72cfd52c2d8197f663389fc50c5"
     logger = get_logger(__name__)
 
     # -- 3. Setup constants
@@ -70,7 +70,7 @@ def get_basics_comp(emb_model, model, default_system_prompt_link, _logger, podca
 
     genre = st.sidebar.radio(
                         "Seleccione el LLM",
-                        ["LLAMA", "GPT (not available yet)"]
+                        ["LLAMA", "GPT"]
                     )
     st.sidebar.info('Modelo LLAMA: ' + str(model).split('/')[-1] + '\nModelo GPT: gpt-3.5-turbo', icon="ℹ️")
     podcast_list = list(podcast_url_video_df['podcast_name_lit'].apply(lambda x: x.replace("'", "")))
@@ -80,7 +80,6 @@ def get_basics_comp(emb_model, model, default_system_prompt_link, _logger, podca
         on_change=clean_chat
     )
 
-    print("Add sidebar")
     # -- Add icons
     with st.sidebar.container():
         st.markdown(
@@ -121,19 +120,18 @@ def get_basics_comp(emb_model, model, default_system_prompt_link, _logger, podca
     video_option_joined_path = "{}_transcription.txt".format(video_option_joined)
     youtube_video_url   = list(podcast_url_video_df[podcast_url_video_df['podcast_name'].str.contains(video_option_joined)]['youtube_video_url'])[0].replace("\'", "")
     st.title("[Podcast: {}]({})".format(video_option.replace("'", "").title(), youtube_video_url))
-    print("Add system prompt file")
+
     # -- 4. Setup request for system prompt
-    with open(default_system_prompt_link, 'r') as f:
-        default_system_prompt = f.read()
-    
+    f = urllib.request.urlopen(default_system_prompt_link)
+    default_system_prompt = str(f.read(), 'UTF-8')
+
     # -- 5. Setup app
-    print("SETUP APP")
-    translator, nlp, retriever = utils.setup_app(video_option_joined_path, emb_model, model, _logger)
+    nlp, retriever = utils.setup_app(video_option_joined_path, emb_model, model, _logger)
 
     # -- 6. Setup model
     together.api_key = os.environ["TOGETHER_API_KEY"]
     #together.Models.start(model)
-    return together, translator, nlp, retriever, video_option, video_option_joined_path, default_system_prompt, youtube_video_url
+    return together, nlp, retriever, video_option, video_option_joined_path, default_system_prompt, youtube_video_url, genre
 
 def clean_chat():
     st.session_state.conversation = None
@@ -154,13 +152,11 @@ def main():
     WIDTH                      = 50
     SIDE                       = (100 - WIDTH) / 2
 
-    print("Loading podcast data")
+    
     podcast_url_video_df = get_podcast_data(PODCAST_URL_VIDEO_PATH)
 
-    print("Loading basic components")
-    together, translator, nlp, retriever, video_option, video_option_joined_path, default_system_prompt, youtube_video_url = get_basics_comp(EMB_MODEL, MODEL, 
-                                                                                                                                             DEFAULT_SYSTEM_PROMPT_LINK, logger, 
-                                                                                                                                             podcast_url_video_df, img_size=100)
+    together, nlp, retriever, video_option, video_option_joined_path, default_system_prompt, youtube_video_url, genre = get_basics_comp(EMB_MODEL, MODEL, DEFAULT_SYSTEM_PROMPT_LINK, logger, 
+                                                                                                podcast_url_video_df, img_size=100)
 
     # -- 6. Setup prompt template + llm chain
     instruction = """CONTEXTO:/n/n {context}/n
@@ -168,8 +164,6 @@ def main():
 PREGUNTA: {question}
 
 RESPUESTA: """
-
-    print("Load prompts")
     prompt_template = utils.get_prompt(instruction, default_system_prompt, B_SYS, E_SYS, B_INST, E_INST, logger)
 
     llama_prompt = PromptTemplate(
@@ -177,10 +171,16 @@ RESPUESTA: """
     )
     chain_type_kwargs = {"prompt": llama_prompt}
 
-    print("Create LLM chain")
     qa_chain = utils.create_llm_chain(MODEL, retriever, chain_type_kwargs, logger, video_option_joined_path)
 
     # ---------------------------------------------------------------------
+    if st.button('Info.'):
+        search = DuckDuckGoSearchRun()
+        character_name = video_option.replace("'", "").title().split("Entrevista A ")[-1]
+        info = search.run("¿Quien es {}?".format(character_name))
+        character_info = utils.get_character_info_gpt(info, character=character_name)
+        st.info(character_info)
+    
     _, container, _ = st.columns([SIDE, WIDTH, SIDE])
     with container:
         st_player(utils.typewrite(youtube_video_url))
@@ -197,24 +197,53 @@ RESPUESTA: """
             st.markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("assistant"):
-            llm_response = qa_chain(prompt)
-            llm_response = utils.process_llm_response(llm_response, nlp)
-            st.markdown(llm_response)
-            start_time_str_list = []; start_time_seconds_list = []; end_time_seconds_list = []
-            for response in llm_response.split('\n'):
-                if re.search(r'(\d{2}:\d{2}:\d{2}(.\d{6})?)', response) != None:
-                    start_time_str, start_time_seconds, _, end_time_seconds = utils.add_hyperlink_and_convert_to_seconds(response)
-                    start_time_str_list.append(start_time_str)
-                    start_time_seconds_list.append(start_time_seconds)
-                    end_time_seconds_list.append(end_time_seconds)
-
-            if start_time_str_list:
-                for start_time_seconds, start_time_str, end_time_seconds in zip(start_time_seconds_list, start_time_str_list, end_time_seconds_list):
-                    st.markdown("__Fragmento: " + start_time_str + "__")
-                    _, container, _ = st.columns([SIDE, WIDTH, SIDE])
-                    with container:
-                        st_player(youtube_video_url.replace("?enablejsapi=1", "") + f'?start={start_time_seconds}&end={end_time_seconds}')
-
+            if 'GPT' not in genre:
+                if prompt.lower() == 'resume':
+                    llm_response = utils.summarise_doc(video_option_joined_path, model_name='llama', model=MODEL)
+                    st.markdown(llm_response)
+                else:
+                    llm_response = qa_chain(prompt)['result']
+                    llm_response = utils.process_llm_response(llm_response)
+                    st.markdown(llm_response)
+        
+                    start_time_str_list = []; start_time_seconds_list = []; end_time_seconds_list = []
+                    for response in llm_response.split('\n'):
+                        if re.search(r'(\d{2}:\d{2}:\d{2}(.\d{6})?)', response) != None:
+                            start_time_str, start_time_seconds, _, end_time_seconds = utils.add_hyperlink_and_convert_to_seconds(response)
+                            start_time_str_list.append(start_time_str)
+                            start_time_seconds_list.append(start_time_seconds)
+                            end_time_seconds_list.append(end_time_seconds)
+        
+                    if start_time_str_list:
+                        for start_time_seconds, start_time_str, end_time_seconds in zip(start_time_seconds_list, start_time_str_list, end_time_seconds_list):
+                            st.markdown("__Fragmento: " + start_time_str + "__")
+                            _, container, _ = st.columns([SIDE, WIDTH, SIDE])
+                            with container:
+                                st_player(youtube_video_url.replace("?enablejsapi=1", "") + f'?start={start_time_seconds}&end={end_time_seconds}')
+            else:
+                if prompt.lower() == 'resume':
+                    llm_response = utils.summarise_doc(video_option_joined_path, model_name='gpt')
+                    st.markdown(llm_response)
+                else:
+                    llm_response = utils.get_gpt_response(video_option_joined_path, prompt, logger)
+                    llm_response = utils.process_llm_response(llm_response)
+                    st.markdown(llm_response)
+        
+                    start_time_str_list = []; start_time_seconds_list = []; end_time_seconds_list = []
+                    for response in llm_response.split('\n'):
+                        if re.search(r'(\d{2}:\d{2}:\d{2}(.\d{6})?)', response) != None:
+                            start_time_str, start_time_seconds, _, end_time_seconds = utils.add_hyperlink_and_convert_to_seconds(response)
+                            start_time_str_list.append(start_time_str)
+                            start_time_seconds_list.append(start_time_seconds)
+                            end_time_seconds_list.append(end_time_seconds)
+        
+                    if start_time_str_list:
+                        for start_time_seconds, start_time_str, end_time_seconds in zip(start_time_seconds_list, start_time_str_list, end_time_seconds_list):
+                            st.markdown("__Fragmento: " + start_time_str + "__")
+                            _, container, _ = st.columns([SIDE, WIDTH, SIDE])
+                            with container:
+                                st_player(youtube_video_url.replace("?enablejsapi=1", "") + f'?start={start_time_seconds}&end={end_time_seconds}')
+                        
         st.session_state.messages.append({"role": "assistant", "content": llm_response})
 # -- Sample: streamlit run app.py -- --DEFAULT_SYSTEM_PROMPT_LINK=https://raw.githubusercontent.com/AlbertoUAH/Castena/main/prompts/default_system_prompt.txt --PODCAST_URL_VIDEO_PATH=https://raw.githubusercontent.com/AlbertoUAH/Castena/main/data/podcast_youtube_video.csv --TRANSCRIPTION=worldcast_roberto_vaquero --MODEL=togethercomputer/llama-2-7b-chat --EMB_MODEL=BAAI/bge-base-en-v1.5
 if __name__ == '__main__':
